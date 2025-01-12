6c47426823b96edb03c62f41d1ed89e92f49e066
alicmerjem <merjem.alic@stu.ibu.edu.ba>
2025-01-12 23:28:01 +0100

    SPA

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,15 +1,23 @@
 function submitForm(e) {
-    e.preventDefault(); // Prevent default form submission
+    e.preventDefault(); 
     const form = e.target;
-    
-    fetch(form.action, {
-        method: 'POST',
-        body: new FormData(form)
-    })
-    .then(response => {
-        if (response.ok) {
-            window.location.href = 'mainpage.html'; // Redirect upon success
-        }
-    })
-    .catch(error => console.error('Error:', error));
+    const email = form.email.value;
+    const password = form.password.value;
+
+    // fetch from json
+    fetch('/json/users.json')
+        .then(response => response.json())
+        .then(usersData => {
+            const user = usersData.find(user => user.email === email && user.password === password);
+
+            if (user) {
+                localStorage.setItem("loggedIn", "true");
+                localStorage.setItem("username", user.username);
+                localStorage.setItem("email", user.email);
+                window.location.href = 'mainpage.html'; 
+            } else {
+                alert("Invalid credentials. Please try again.");
+            }
+        })
+        .catch(error => console.error('Error:', error));
 }
